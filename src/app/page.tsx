28383e1cecda19e54254ef962fd36958933be942
AnonyXcali <sauravezio@gmail.com>
2024-11-27 23:44:51 +0100

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,125 @@
-import Image from "next/image";
+'use client'
+import { useEffect, useState } from "react"
+
+const URL = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
+
+/**
+ * 
+ * response example
+ * 
+ * {"by":"dhouston",
+ * "descendants":71,"id":8863,"kids":
+ * [9224,8917,8884,8887,8952,8869,8873,8958,8940,8908,9005,9671,9067,9055,
+ * 8865,8881,8872,8955,10403,8903,8928,9125,8998,8901,8902,8907,8894,8870,8878
+ * ,8980,8934,8943,8876],"score":104,
+ * "time":1175714200,
+ * "title":"My YC app: Dropbox - Throw away your USB drive","type":"story"
+ * ,"url":"http://www.getdropbox.com/u/2/screencast.html"}
+ */
+
+type Story = {
+  by: string,
+  descendants: number,
+  id: number,
+  kids?: Array<number>,
+  score: number,
+  time: number,
+  title: string,
+  type: string,
+  url: string,
+}
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [isFetching, setFetching] = useState(false)
+  const [ids, setIds] = useState<Array<number>>([])
+  const [stories, setStories] = useState<Story[] | undefined>()
+  const [paginationValue, setPagination] = useState<number>(5)
+  const [offset, setOffset] = useState<{
+    start: number,
+    end: number,
+  }>({
+    start: 0,
+    end: 5,
+  })
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    //calling api
+    const fetchData = async () => {
+      const res = await fetch(URL)
+      const response: Array<number> = await res.json()
+      // https://hacker-news.firebaseio.com/v0/item/8863.json
+      const fetchedStories: Array<Story> = []
+
+      if(response && response.length >= 0) {
+        setIds(response)
+        const pageinatedIds = response.slice(offset.start, offset.end)
+        //iterate over the array and fetch the resulting stories
+        for(const id of pageinatedIds) {
+          const res = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+          const storiesFetched = await res.json()
+          fetchedStories.push(storiesFetched)
+        }
+        setStories(fetchedStories)
+      }
+      setFetching(false)
+    }
+
+    setFetching(true)
+    fetchData()
+  }, [offset])
+
+
+  const handleOnSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setPagination(parseInt(e.target.value, 10))
+  }
+
+  const handleChangeOfIds = () => {
+    const updated = {
+      start: offset.start + paginationValue,
+      end: offset.end + paginationValue,
+    }
+    setOffset(updated)
+  }
+
+
+  return (
+    <div>
+      {isFetching ? (
+        <p>Loading</p>
+      ) : (
+        <div>
+          <div>
+            <select
+              onChange={handleOnSelect}
+              value={paginationValue}
+            >
+              <option value={5}>5</option>
+              <option value={10}>10</option>
+              <option value={20}>20</option>
+            </select>
+
+          </div>
+          <div>
+            {stories && stories.map((item) => (
+              <div
+                key={item.id}
+              >
+                <p>{item.title}</p>
+              </div>
+            ))}
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      )}
+
+      <div>
+        <button
+          type="button"
+          onClick={handleChangeOfIds}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
